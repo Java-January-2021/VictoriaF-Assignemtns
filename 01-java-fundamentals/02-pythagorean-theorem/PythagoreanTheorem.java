9b2d307f3c2c00446471f84d62bd81df777ba7ec
vicfuen94 <fuentesvictoria923@gmail.com>
2021-01-13 22:02:53 -0600

    pythagorean theorem

---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythagoreanTheorem.java
@@ -0,0 +1,8 @@
+public class PythagoreanTheorem {
+    public static void main(String[] args) {
+        public static double calculateHypotenuse (int lenA, int lenB) {
+                    return Math.sqrt((lenA * lenA) + (lenB * lenB));
+                }
+    }
+        
+}
